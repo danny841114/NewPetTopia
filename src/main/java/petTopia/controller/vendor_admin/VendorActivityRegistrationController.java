40c9942cf4afd0c18361ad6823a8952cb2c1ffa1
Danny Lin <danny841114@gmail.com>
2026-07-23 08:32:17 +0000

    refactor vendor activity registration controller

--- src/main/java/petTopia/controller/vendor_admin/VendorActivityRegistrationController.java
+++ src/main/java/petTopia/controller/vendor_admin/VendorActivityRegistrationController.java
@@ -1,156 +1,70 @@
 package petTopia.controller.vendor_admin;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import petTopia.model.vendor.ActivityPeopleNumber;
 import petTopia.model.vendor.ActivityRegistration;
-import petTopia.model.vendor.Notification;
-import petTopia.repository.vendor.NotificationRepository;
-import petTopia.repository.vendor_admin.ActivityPeopleNumberRepository;
-import petTopia.repository.vendor_admin.ActivityRegistrationRepository;
+import petTopia.service.vendor_admin.VendorActivityRegistrationService;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/vendor_admin")
 public class VendorActivityRegistrationController {
-    private final ActivityRegistrationRepository activityRegistrationRepository;
-    private final NotificationRepository notificationRepository;
-    private final ActivityPeopleNumberRepository activityPeopleNumberRepository;
+    private final VendorActivityRegistrationService vendorActivityRegistrationService;
 
     @GetMapping("/activity/registration")
-    public ResponseEntity<?> getRegistrationByActivityId(@RequestParam Integer activityId) {
-        List<ActivityRegistration> registration = activityRegistrationRepository.findByVendorActivityId(activityId);
-
-        if (registration.isEmpty()) return ResponseEntity.ok(Collections.emptyList());
-
-        return ResponseEntity.ok(registration);
+    public ResponseEntity<?> getRegistrations(@RequestParam Integer activityId) {
+        List<ActivityRegistration> registrations = vendorActivityRegistrationService.getRegistrationsByActivityId(activityId);
+        return ResponseEntity.ok(registrations);
     }
 
-    // TODO: Move to service layer
+    // TODO: Response body should be modified
     @PutMapping("/activity/confirmAll")
     public ResponseEntity<?> confirmAllRegistrations(@RequestParam Integer activityId) {
-        List<ActivityRegistration> registrations = activityRegistrationRepository.findByVendorActivityId(activityId);
-
-        if (registrations.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No registrations found.");
-
-        // 批量更新状态为 "confirmed"
-        registrations.forEach(registration -> {
-            registration.setStatus("confirmed");
-            activityRegistrationRepository.save(registration);
-        });
-
+        vendorActivityRegistrationService.confirmAllRegistrations(activityId);
         return ResponseEntity.ok("All registrations have been confirmed and notifications sent.");
     }
 
-    // TODO: Move to service layer
     @PutMapping("/registration/update/{id}")
     public ResponseEntity<?> updateRegistrationStatus(@PathVariable Integer id, @RequestBody String status) {
-        Optional<ActivityRegistration> optional = activityRegistrationRepository.findById(id);
-
-        if (optional.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No registrations found.");
-
-        ActivityRegistration registration = optional.get();
-
-        registration.setStatus(status);
-        ActivityRegistration savedRegistration = activityRegistrationRepository.save(registration);
-
-        return ResponseEntity.ok(savedRegistration);
+        ActivityRegistration registration = vendorActivityRegistrationService.updateRegistrationStatus(id, status);
+        return ResponseEntity.ok(registration);
     }
 
-    // TODO: Move to service layer
+    // TODO: Response body should be modified
     @PutMapping("/registration/confirmById/{registrationId}")
-    public ResponseEntity<?> confirmRegistrationById(@PathVariable Integer registrationId) {
-        ActivityRegistration registration = activityRegistrationRepository.findById(registrationId).orElse(null);
-
-        if (registration == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registration not found.");
-
-        Integer activityId = registration.getVendorActivity().getId();
-        Optional<ActivityPeopleNumber> current = activityPeopleNumberRepository.findByVendorActivityId(activityId);
-
-        if (current.isEmpty())
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ActivityPeopleNumber not found.");
-
-        registration.setStatus("confirmed");
-        activityRegistrationRepository.save(registration);
-
-        ActivityPeopleNumber peopleNumber = current.get();
-        peopleNumber.setCurrentParticipants(peopleNumber.getCurrentParticipants() + 1);
-        activityPeopleNumberRepository.save(peopleNumber);
-
+    public ResponseEntity<?> confirmRegistration(@PathVariable Integer registrationId) {
+        vendorActivityRegistrationService.confirmRegistrationById(registrationId);
         return ResponseEntity.ok("Registration confirmed and notification sent.");
     }
 
-    // TODO: Move to service layer
+    // TODO: Response body should be modified
     @PutMapping("/registration/cancelById/{registrationId}")
-    public ResponseEntity<?> cancelRegistrationById(@PathVariable Integer registrationId) {
-        ActivityRegistration registration = activityRegistrationRepository.findById(registrationId).orElse(null);
-
-        if (registration == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registration not found.");
-
-        registration.setStatus("canceled");
-        activityRegistrationRepository.save(registration);
-
+    public ResponseEntity<?> cancelRegistration(@PathVariable Integer registrationId) {
+        vendorActivityRegistrationService.cancelRegistrationById(registrationId);
         return ResponseEntity.ok("Registration canceled and notification sent.");
     }
 
 
-    // TODO: Move to service layer
+    // TODO: Response body should be modified
     @DeleteMapping("/registration/deleteById/{registrationId}")
     public ResponseEntity<?> deleteRegistrationById(@PathVariable Integer registrationId) {
-        ActivityRegistration registration = activityRegistrationRepository.findById(registrationId).orElse(null);
-
-        if (registration == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registration not found.");
-
-        Integer activityId = registration.getVendorActivity().getId();
-        Optional<ActivityPeopleNumber> current = activityPeopleNumberRepository.findByVendorActivityId(activityId);
-
-        if (current.isEmpty())
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ActivityPeopleNumber not found.");
-
-        if ("confirmed".equals(registration.getStatus())) {
-            ActivityPeopleNumber peopleNumber = current.get();
-            peopleNumber.setCurrentParticipants(peopleNumber.getCurrentParticipants() - 1);
-            activityPeopleNumberRepository.save(peopleNumber);
-        }
-
-        Integer memberId = registration.getMember().getId();
-        notificationRepository.findByMemberId(memberId).forEach(notificationRepository::delete);
-
-        activityRegistrationRepository.delete(registration);
-
+        vendorActivityRegistrationService.deleteRegistrationById(registrationId);
         return ResponseEntity.ok("Registration canceled and notifications sent.");
     }
 
-    // TODO: Move to service layer
+    // TODO:
+    //  Request body need to be fixed
+    //  Response body should be modified
     @PostMapping("/registration/notification/{memberId}/{activityId}")
     public ResponseEntity<?> sendNotification(@PathVariable Integer memberId,
                                               @PathVariable Integer activityId,
                                               @RequestParam String title,
                                               @RequestParam String content) {
-        Optional<ActivityRegistration> registrationOpt = activityRegistrationRepository.findByMemberIdAndVendorActivityId(memberId, activityId);
-
-        if (registrationOpt.isPresent()) {
-            ActivityRegistration registration = registrationOpt.get();
-
-            Notification notification = new Notification();
-            notification.setMember(registration.getMember());
-            notification.setVendor(registration.getVendorActivity().getVendor());
-            notification.setVendorActivity(registration.getVendorActivity());
-            notification.setNotificationTitle(title);
-            notification.setNotificationContent(content);
-
-            notificationRepository.save(notification);
-
-            return ResponseEntity.ok("Notification sent.");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Activity registration not found.");
-        }
+        vendorActivityRegistrationService.sendNotification(memberId, activityId, title, content);
+        return ResponseEntity.ok("Notification sent.");
     }
 }
---
+++ src/main/java/petTopia/service/vendor_admin/VendorActivityRegistrationService.java
@@ -0,0 +1,115 @@
+package petTopia.service.vendor_admin;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import petTopia.model.vendor.ActivityPeopleNumber;
+import petTopia.model.vendor.ActivityRegistration;
+import petTopia.model.vendor.Notification;
+import petTopia.repository.vendor.NotificationRepository;
+import petTopia.repository.vendor_admin.ActivityPeopleNumberRepository;
+import petTopia.repository.vendor_admin.ActivityRegistrationRepository;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class VendorActivityRegistrationService {
+    private final ActivityRegistrationRepository activityRegistrationRepository;
+    private final NotificationRepository notificationRepository;
+    private final ActivityPeopleNumberRepository activityPeopleNumberRepository;
+
+    public List<ActivityRegistration> getRegistrationsByActivityId(Integer activityId) {
+        return activityRegistrationRepository.findByVendorActivityId(activityId);
+    }
+
+    @Transactional
+    public void confirmAllRegistrations(Integer activityId) {
+        List<ActivityRegistration> registrations = activityRegistrationRepository.findByVendorActivityId(activityId);
+
+        if (registrations.isEmpty()) throw new EntityNotFoundException("Registrations not found");
+
+        registrations.forEach(registration -> registration.setStatus("confirmed"));
+        activityRegistrationRepository.saveAll(registrations);
+    }
+
+    @Transactional
+    public ActivityRegistration updateRegistrationStatus(Integer registrationId, String status) {
+        ActivityRegistration registration = getActivityRegistrationById(registrationId);
+
+        registration.setStatus(status);
+
+        return activityRegistrationRepository.save(registration);
+    }
+
+    @Transactional
+    public void confirmRegistrationById(@PathVariable Integer registrationId) {
+        ActivityRegistration registration = getActivityRegistrationById(registrationId);
+
+        Integer activityId = registration.getVendorActivity().getId();
+
+        ActivityPeopleNumber peopleNumber = getActivityPeopleNumberById(activityId);
+
+        registration.setStatus("confirmed");
+        activityRegistrationRepository.save(registration);
+
+        peopleNumber.setCurrentParticipants(peopleNumber.getCurrentParticipants() + 1);
+        activityPeopleNumberRepository.save(peopleNumber);
+    }
+
+    @Transactional
+    public void cancelRegistrationById(Integer registrationId) {
+        ActivityRegistration registration = getActivityRegistrationById(registrationId);
+
+        registration.setStatus("canceled");
+
+        activityRegistrationRepository.save(registration);
+    }
+
+    @Transactional
+    public void deleteRegistrationById(Integer registrationId) {
+        ActivityRegistration registration = getActivityRegistrationById(registrationId);
+
+        Integer activityId = registration.getVendorActivity().getId();
+
+        ActivityPeopleNumber peopleNumber = getActivityPeopleNumberById(activityId);
+
+        if ("confirmed".equals(registration.getStatus())) {
+            peopleNumber.setCurrentParticipants(peopleNumber.getCurrentParticipants() - 1);
+            activityPeopleNumberRepository.save(peopleNumber);
+        }
+
+        Integer memberId = registration.getMember().getId();
+        notificationRepository.findByMemberId(memberId).forEach(notificationRepository::delete);
+
+        activityRegistrationRepository.delete(registration);
+    }
+
+    @Transactional
+    public void sendNotification(Integer memberId, Integer activityId, String title, String content) {
+        ActivityRegistration registration = activityRegistrationRepository.findByMemberIdAndVendorActivityId(memberId, activityId)
+                .orElseThrow(() -> new EntityNotFoundException("Activity registration not found"));
+
+        Notification notification = new Notification();
+        notification.setMember(registration.getMember());
+        notification.setVendor(registration.getVendorActivity().getVendor());
+        notification.setVendorActivity(registration.getVendorActivity());
+        notification.setNotificationTitle(title);
+        notification.setNotificationContent(content);
+
+        notificationRepository.save(notification);
+    }
+
+    private ActivityRegistration getActivityRegistrationById(Integer registrationId) {
+        return activityRegistrationRepository.findById(registrationId)
+                .orElseThrow(() -> new EntityNotFoundException("Activity registration not found"));
+    }
+
+    private ActivityPeopleNumber getActivityPeopleNumberById(Integer activityId) {
+        return activityPeopleNumberRepository.findByVendorActivityId(activityId)
+                .orElseThrow(() -> new EntityNotFoundException("Activity people number not found"));
+    }
+}
